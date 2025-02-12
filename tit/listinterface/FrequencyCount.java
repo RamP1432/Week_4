5fa9ecb85352beffb765750db5552985d4ebea7f
Ram Prakash Pathak <133332560+RamP1432@users.noreply.github.com>
2025-02-12 13:27:06 +0530

    Ram Prakash Pathak : List Interface-Frequency count of each element.

---
+++ tit/listinterface/FrequencyCount.java
@@ -0,0 +1,19 @@
+package com.tit.listinterface;
+
+import java.util.*;
+
+public class FrequencyCount {
+    // Method to find frequency of elements in a list
+    public static Map<String, Integer> findFrequency(List<String> list) {
+        Map<String, Integer> frequencyMap = new HashMap<>();
+        for (String item : list) {
+            frequencyMap.put(item, frequencyMap.getOrDefault(item, 0) + 1);
+        }
+        return frequencyMap;
+    }
+
+    public static void main(String[] args) {
+        List<String> stringList = Arrays.asList("apple", "banana", "apple", "orange");
+        System.out.println("Frequency of elements: " + findFrequency(stringList));
+    }
+}
