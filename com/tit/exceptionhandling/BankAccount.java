ac7fda1402690aeb3e90b0c14ff03318070e7ee1
Ram Prakash Pathak <133332560+RamP1432@users.noreply.github.com>
2025-02-14 16:45:43 +0530

    [Ram Prakash Pathak] Checked Exception (Compile-time Exception)

---
+++ com/tit/exceptionhandling/BankAccount.java
@@ -0,0 +1,48 @@
+package com.tit.exceptionhandling;
+
+import java.util.Scanner;
+
+// Custom exception for insufficient balance
+class InsufficientBalanceException extends Exception {
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
+
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public void withdraw(double amount) throws InsufficientBalanceException , IllegalArgumentException {
+        // Check for the negative amount
+        if (amount < 0) {
+            throw new IllegalArgumentException("Invalid amount!");
+        }
+
+        // Check for insufficient balance
+        if (amount > balance) {
+            throw new InsufficientBalanceException("Insufficient balance!");
+        }
+
+        // Perform withdrawal
+        balance -= amount;
+        System.out.println("Withdrawal successful, new balance: " + balance);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        BankAccount account = new BankAccount(1000.00); // Initial balance of 1000
+        System.out.println("Enter withdraw amount: ");
+        double amountToWithdraw = sc.nextDouble(); // Amount to withdraw
+        try {
+            account.withdraw(amountToWithdraw); // Attempt to withdraw
+        } catch (InsufficientBalanceException e) {
+            System.out.println(e.getMessage()); // Handle insufficient balance
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage()); // Handle invalid amount
+        }
+    }
+}
