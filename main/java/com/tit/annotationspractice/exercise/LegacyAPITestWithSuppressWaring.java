0f359c66fbcca8fc9aa24e0ef11c4e153df6baab
Ram Prakash Pathak <133332560+RamP1432@users.noreply.github.com>
2025-02-17 02:44:01 +0530

    [Ram Prakash Pathak] Suppress Unchecked Warnings.

--- main/java/com/tit/annotationspractice/exercise/LegacyAPITestWithSuppressWaring.java
+++ main/java/com/tit/annotationspractice/exercise/LegacyAPITestWithSuppressWaring.java
@@ -1,25 +1,11 @@
-package com.tit.annotationpractice;
+package com.tit.annotationspractice.exercise;
 
 import java.util.ArrayList;
-
-class LegacyAPI2 {
-    @Deprecated
-    public void oldFeature() {
-        System.out.println("This is the old feature. Please use newFeature() instead.");
-    }
-
-    public void newFeature() {
-        System.out.println("This is the new and improved feature.");
-    }
-}
 
 public class LegacyAPITestWithSuppressWaring {
     @SuppressWarnings("unchecked")
     public static void main(String[] args) {
-        LegacyAPI api = new LegacyAPI();
-
-        api.oldFeature(); // This should show a warning
-        api.newFeature();
+        @SuppressWarnings("unchecked")
         ArrayList list = new ArrayList(); // Creating an ArrayList without generics
         list.add("Test");
         list.add(123); // Mixing types
