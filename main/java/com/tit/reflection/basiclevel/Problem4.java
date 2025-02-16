a92a30a27c986e5d0deff58709fcee38d71c18b4
Ram Prakash Pathak <133332560+RamP1432@users.noreply.github.com>
2025-02-17 03:42:29 +0530

    [Ram Prakash Pathak] Dynamically Create Objects.

---
+++ main/java/com/tit/reflection/basiclevel/Problem4.java
@@ -0,0 +1,48 @@
+package com.tit.reflection.basiclevel;
+
+import java.lang.reflect.Constructor;
+
+class Student {
+    private String name;
+    private int rollNumber;
+
+    // Constructor
+    public Student(String name, int rollNumber) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{name='" + name + "', rollNumber=" + rollNumber + '}';
+    }
+}
+
+public class Problem4 {
+    public static void main(String[] args) {
+        try {
+            // Get the Student class
+            Class<Student> studentClass = Student.class;
+
+            // Get the constructor of the Student class
+            Constructor<Student> constructor = studentClass.getConstructor(String.class, int.class);
+
+            // Create an instance of Student class using the constructor
+            Student student = constructor.newInstance("John Doe", 101);
+
+            // Display the created instance
+            System.out.println("Created Student instance: " + student);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
