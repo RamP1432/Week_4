c8c0fcaf8456b7095d7eeb85c950b7ed440eedb0
Ram Prakash Pathak <133332560+RamP1432@users.noreply.github.com>
2025-02-17 01:36:22 +0530

    [Ram Prakash Pathak] Use @Deprecated to Mark an Old Method

---
+++ main/java/com/tit/annotationpractice/LegacyAPITest.java
@@ -0,0 +1,21 @@
+package com.tit.annotationpractice;
+
+class LegacyAPI {
+    @Deprecated
+    public void oldFeature() {
+        System.out.println("This is the old feature. Please use newFeature() instead.");
+    }
+
+    public void newFeature() {
+        System.out.println("This is the new and improved feature.");
+    }
+}
+
+public class LegacyAPITest {
+    public static void main(String[] args) {
+        LegacyAPI api = new LegacyAPI();
+
+        api.oldFeature(); // This should show a warning
+        api.newFeature();
+    }
+}
