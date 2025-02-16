c4330eda53296bf0b1d2e9b2143ae42fa6b3fea2
Ram Prakash Pathak <133332560+RamP1432@users.noreply.github.com>
2025-02-17 00:37:15 +0530

    [Ram Prakash Pathak] Code of Even checker and it's testing.

---
+++ main/java/com/tit/basicjunittest/EvenChecker.java
@@ -0,0 +1,9 @@
+package com.tit.basicjunittest;
+
+public class EvenChecker {
+
+    // Returns true if the number is even
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
